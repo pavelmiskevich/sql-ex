933a0f644dcfd9df903e9f5b6663517a0f559c6f
pavelmiskevich <p_a_v_l_i_k@mail.ru>
2020-07-08 10:58:03 +0300

    add new

--- SQL/70 (2) Корабли.sql
+++ SQL/70 (2) Корабли.sql
@@ -1,9 +1,8 @@
-”кажите сражени¤, в которых участвовало по меньшей мере три корабл¤ одной и той же страны.
-ѕредложение GROUP BY
-¬нешние соединени¤
-ѕредложение HAVING
-------------------------------------------------------------------------
--- сражени¤, в которых участвовало по меньшей мере три корабл¤
+--Укажите сражения, в которых участвовало по меньшей мере три корабля одной и той же страны.
+--    Предложение GROUP BY
+--    Внешние соединения
+--    Предложение HAVING
+-- Процесс -------------------------------------------------------------
 select o.battle, count(o.ship) from Outcomes o
 group by o.battle
 having count(o.ship) >= 3
@@ -90,9 +89,23 @@ where o.ship NOT IN (select name from Ships)
 group by r.country, r.battle
 having count(*) >= 3
 
--- ѕопробовать FULL JOIN
+select c.country, o.battle from Outcomes o
+left join Ships s on s.name = o.ship
+left join Classes c on o.ship = c.class OR s.class = c.class
+where c.country IS NOT NULL
 
-------------------------------------------------------------------------
+
+-- Решение -------------------------------------------------------------
+select distinct o.battle from Outcomes o
+left join Ships s on s.name = o.ship
+left join Classes c on o.ship = c.class OR s.class = c.class
+where c.country IS NOT NULL
+group by c.country, o.battle
+having count(o.battle) >= 3
+
+--cost	0.051127400249243
+--operations	10
+
 -- GIT HUB
 SELECT DISTINCT Outcomes.battle 
 FROM Outcomes 
@@ -101,3 +114,16 @@ LEFT JOIN Classes ON Outcomes.ship = Classes.class OR Ships.class = Classes.clas
 WHERE Classes.country IS NOT NULL 
 GROUP BY Classes.country, Outcomes.battle 
 HAVING COUNT(Outcomes.ship) >= 3
+
+--cost	0.051127400249243
+--operations	10
+
+--FORUM
+--https://www.sql-ex.ru/forum/Lforum.php?F=3&N=70#20
+Select distinct battle from (
+Select coalesce(class,ship) class, battle from outcomes o left join ships s on o.ship = s.name) bc join classes on bc.class = classes.class
+group by country, battle
+having count(country) > =3
+
+--cost	0.043022762984037
+--operations	10
