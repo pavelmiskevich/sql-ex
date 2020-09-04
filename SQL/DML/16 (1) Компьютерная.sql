a5328c6530221814a30cfe7b8df8515e46666b32
pavelmiskevich <p_a_v_l_i_k@mail.ru>
2020-09-04 10:09:17 +0300

    add new DML/16 (1) Компьютерная.sql

---
+++ SQL/DML/16 (1) Компьютерная.sql
@@ -0,0 +1,16 @@
+--Удалить из таблицы Product те модели, которые отсутствуют в других таблицах.
+--    Оператор DELETE
+-- Процесс -------------------------------------------------------------
+
+-- Решение -------------------------------------------------------------
+DELETE FROM Product 
+	FROM Product p 
+	LEFT JOIN PC ON p.model = PC.model 
+	LEFT JOIN Laptop l ON p.model = l.model 
+	LEFT JOIN Printer pr ON p.model = pr.model 
+	WHERE PC.model IS NULL AND l.model IS NULL AND pr.model IS NULL
+
+-- GIT HUB
+
+--FORUM
+--https://www.sql-ex.ru/forum/forum.php?F=2&N=15#19
